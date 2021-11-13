2edf376ab0f89c6c9464da570ac740c838879b69
Cruel Efficiency <cruel.efficiency@gmail.com>
2021-11-12 19:15:23 -0800

    Updating the algo to use a display name instead of the code name of heroes in the scraping tool.

--- hero-image-scraper/parse-heroes.py
+++ hero-image-scraper/parse-heroes.py
@@ -9,16 +9,17 @@ for line in f_dotabuff_html:
 f_ids = open("heroes-ids.txt", "r")
 
 hero_id_objs = json.loads(f_ids.read())
-name_to_id = {}
+name_to_obj = {}
 for hero_id_obj in hero_id_objs["heroes"]:
-    name_to_id[hero_id_obj["name"]] = hero_id_obj["id"]
+    name_to_obj[hero_id_obj["name"]] = hero_id_obj
 
 heroes_dict = {}
 for url in image_urls:
     hero_name = re.search("\/assets\/heroes\/(.{1,23})-", url).group(1)
-    heroes_dict[str(name_to_id[hero_name])] = {
-        "id": name_to_id[hero_name],
-        "name": hero_name,
+    hero_obj = name_to_obj[hero_name]
+    heroes_dict[str(hero_obj["name"])] = {
+        "id": hero_obj["id"],
+        "name": hero_obj["localized-name"],
         "image": "http://dotabuff.com" + url
     }
 print(heroes_dict)
